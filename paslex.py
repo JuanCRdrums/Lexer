6cc860d76d2673ef688abda1c1a47f0328731b17
Juan Camilo Rojas <juankdrums14@gmail.com>
2018-08-21 14:36:38 -0500

    Se agregan palabras reservadas a los tokens

--- paslex.py
+++ paslex.py
@@ -2,12 +2,21 @@ from sly import Lexer
 
 class PascalLexer(Lexer):
 
-    tokens = { ID, INTCONST, CHARCONST, SPECIAL, PREID }
+    keywords = { 'program', 'var', 'array', 'of', 'procedure', 'begin', 'end',
+                'write', 'read', 'if', 'then', 'else', 'while', 'do', 'not',
+                'or', 'div', 'and'}
+    tokens = { ID, INTCONST, CHARCONST, *{kw.upper() for kw in self.keywords},
+                PLUS, MINUS, TIMES, EQ, NE, LT, GT, LE, GE, LPAR, RPAR, LBR,
+                RBR, ASSIGN, DOT, COMA, SEMICOLON, COLON, RANGE }
 
     ignore_comment = r'\(\*.*\*\)|{.*}|//.*'
     ignore_newline = r'\n+'
 
-    ID = r'[a-zA-z][a-zA-Z0-9]*'
     INTCONST = r'[0-9]+'
     CHARCONST = r'\'.*\'|".*"'
-    SPECIAL = r''
+
+    @_r'[a-zA-z][a-zA-Z0-9]*'
+    def ID(self,t):
+        if t.value.lower in self.keywords:
+            t.type = t.value.upper()
+        return t 
